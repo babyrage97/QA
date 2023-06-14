5a1fe2132f8a376fc2a1db79977a20d9f9c6d232
milanpaunovic <50558440+babyrage97@users.noreply.github.com>
2023-06-14 21:50:14 +0200

    shortpw

--- 3. Scripts/user_page.rb
+++ 3. Scripts/user_page.rb
@@ -3,6 +3,7 @@ class UserPage
 
   #css selectors
  SUCCESS_REG = {xpath: "/html/body/div/div/div/div/div/p[1]"}
+ SHORTPW_BOX = {xpath: "/html/body/div[1]/div/div/div/div/div/div/div[1]/form/div[3]/div[1]"}
 
 
   attr_reader :driver
@@ -15,6 +16,12 @@ class UserPage
     success_reg = @driver.find_element(SUCCESS_REG)
     success_reg.text
   end
+  
+  def user_shortpw()
+    shortpw_box = @driver.find_element(SHORTPW_BOX)
+    shortpw_box.text
+  end
+
 
 
 
